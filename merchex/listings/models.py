e699a3461d5b17dc4f741840a95f5c0849cd7854
Mydmoov <denizot.myriam@gmail.com>
2022-11-22 14:24:12 +0100

    Avancement Utilisation admin

--- merchex/listings/models.py
+++ merchex/listings/models.py
@@ -18,12 +18,19 @@ le charfield est un champ de texte qui peut contenir jusqu'à 255 caractères.
 commentaire rajout
 """
 class Band(models.Model):
+    def __str__(self):
+        return f'{self.name}'
 
     class Genre(models.TextChoices):
         HIP_HOP = 'HH'
         SYNTH_POP = 'SP'
         ALTERNATIVE_ROCK = 'AR'
 
+    class Type(models.TextChoices):
+        Records = 'R'
+        Clothing = 'c'
+        Poster = 'P'
+        Miscellaneous = 'M'
 
     name = models.fields.CharField(max_length=100)
     genre = models.fields.CharField(choices=Genre.choices, max_length=5)
@@ -33,7 +40,17 @@ class Band(models.Model):
     )
     active = models.fields.BooleanField(default=True)
     official_homepage = models.fields.URLField(null=True, blank=True)
+    description = models.fields.TextField(null=True, blank=True)
+    sold = models.fields.BooleanField(default=False)
+    year = models.fields.IntegerField(
+    validators=[MinValueValidator(1900), MaxValueValidator(2022)]
+    )
+    type = models.fields.CharField(choices=Type.choices, max_length=5)
 
 
 class Title(models.Model):
     title = models.fields.CharField(max_length=100)
+
+
+
+
